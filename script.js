c529fdeb2b2d5acf96683f57587c0a0e47197cc8
A.Westwood <personal.email.aw1@gmail.com>
2024-05-20 13:34:18 +0100

    05. create variables for menu, options, selected

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach(dropdown => {
     const select = dropdown.querySelector('.select');
     const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelector('.menu li');
+    const selected = dropdown.querySelector('.selected');
 });
