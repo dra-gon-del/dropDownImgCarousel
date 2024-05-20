f3e6cf28f1781336a0f5e8b35c5b4a1b6553b493
A.Westwood <personal.email.aw1@gmail.com>
2024-05-20 13:32:12 +0100

    04. create variables for dropdown, caret, select

---
+++ script.js
@@ -0,0 +1,6 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+});
